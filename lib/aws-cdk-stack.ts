ed6c086a78b075af1251e2efc763d1356c835c4d
RuanRDM <ruan.delatorre.rd@gmail.com>
2023-09-25 20:24:02 -0300

    att

--- lib/aws-cdk-stack.ts
+++ lib/aws-cdk-stack.ts
@@ -1,5 +1,11 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import {Bucket, BucketAccessControl} from "aws-cdk-lib/aws-s3";
+import {BucketDeployment, Source} from "aws-cdk-lib/aws-s3-deployment";
+import * as path from "path";
+import {Distribution, OriginAccessIdentity} from "aws-cdk-lib/aws-cloudfront";
+import {S3Origin} from "aws-cdk-lib/aws-cloudfront-origins";
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsCdkStack extends cdk.Stack {
@@ -12,5 +18,27 @@ export class AwsCdkStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'AwsCdkQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    const bucket = new Bucket(this, 'Bucket', {
+      accessControl: BucketAccessControl.PRIVATE,
+    })
+
+    new BucketDeployment(this, 'BucketDeployment', {
+      destinationBucket: bucket,
+      sources: [Source.asset(path.resolve(__dirname, './dist'))]
+    })
+    
+    const originAccessIdentity = new OriginAccessIdentity(this, 'OriginAccessIdentity');
+    bucket.grantRead(originAccessIdentity);
+    
+    new Distribution(this, 'Distribution', {
+      defaultRootObject: 'index.html',
+      defaultBehavior: {
+        origin: new S3Origin(bucket, {originAccessIdentity}),
+      },
+    })
+
   }
 }
+
+
